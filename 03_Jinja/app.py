89b772ecb79b7c1addf088be9c1b4632b9d5a705
LAshi <lashi0812@gmail.com>
2023-03-05 12:02:49 +0530

    jinja expression

--- 03_Jinja/app.py
+++ 03_Jinja/app.py
@@ -7,3 +7,15 @@ def jinja():
     return render_template("jinja_intro.html",
                            name="Lakshman",
                            template="Jinja")
+    
+@app.route("/expressions/")
+def render_expression():
+    kwargs = dict(color="brown",
+                  animal_one="fox",
+                  animal_two="dog",
+                  orange_amount=10,
+                  apple_amount=20,
+                  donate_amount=15,
+                  first_name="Captain",
+                  last_name="Marvel")
+    return render_template("expression.html",**kwargs)
